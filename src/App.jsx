2138c2b341fed9a4f1459be27ee9862e3cb68223
dessctras <dessctras@gmail.com>
2023-06-07 18:21:40 -0300

    last changes for now

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './App.css'
-import {Contact, Experience, NavBar, SkillSet, Home, Landing,  resume, profile, henry,/*  image1, image2, 
+import {Contact, Experience, NavBar, SkillSet, Home, Landing, /*  image1, image2, 
   image4, image5, image6, image7, image8, image9, image11, image12, image13, image14, image15, image16, image17, image18, image19, image20, image21, image22, image23, */ MiddleLanding, MiddleHome, MiddleContact, Footer} from './components/views.js'
 import { Parallax, ParallaxLayer} from '@react-spring/parallax'
 import { useInView } from 'react-intersection-observer'
@@ -35,9 +35,9 @@ return (
         <MiddleLanding/>
       </ParallaxLayer>  
       
-      <ParallaxLayer offset={1} speed={1}>
+      {/* <ParallaxLayer offset={1} speed={1}>
          <img src={profile} alt="que guebo nojoda" width='300px'/>
-      </ParallaxLayer>
+      </ParallaxLayer> */}
 
       
       <ParallaxLayer  offset={1} speed={1}>
@@ -60,9 +60,9 @@ return (
         </div>
       </ParallaxLayer>
 
-      <ParallaxLayer offset={2} speed={0.5}>
+      {/* <ParallaxLayer offset={2} speed={0.5}>
         <img src={henry} alt="" width='300px'/>
-     </ParallaxLayer> 
+     </ParallaxLayer>  */}
       {/* <ParallaxLayer  offset={2} speed={0.5}>
         <figure>
          <img src={image4} alt=""/>
