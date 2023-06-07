1058f3ecc76b43ca6c330ea1e20424548990a090
dessctras <dessctras@gmail.com>
2023-06-07 18:11:05 -0300

    prueba img 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './App.css'
-import {Contact, Experience, NavBar, SkillSet, Home, Landing,  image1,/* image2, 
+import {Contact, Experience, NavBar, SkillSet, Home, Landing,  resume, profile,/*  image1, image2, 
   image4, image5, image6, image7, image8, image9, image11, image12, image13, image14, image15, image16, image17, image18, image19, image20, image21, image22, image23, */ MiddleLanding, MiddleHome, MiddleContact, Footer} from './components/views.js'
 import { Parallax, ParallaxLayer} from '@react-spring/parallax'
 import { useInView } from 'react-intersection-observer'
@@ -36,7 +36,7 @@ return (
       </ParallaxLayer>  
       
       <ParallaxLayer offset={1} speed={1}>
-         <img src={image1} alt="que guebo nojoda"/>
+         <img src={profile} alt="que guebo nojoda"/>
       </ParallaxLayer>
 
       {/* <ParallaxLayer offset={1} speed={0.5}>
