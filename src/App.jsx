fff70ee287d4ffb4bdd5cf56ee583196a16b7daf
marigabyfc <marianag_flores@hotmail.com>
2023-06-12 13:44:01 -0300

    prueba con dos img

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './App.css'
 import {Contact, Experience, NavBar, SkillSet, Home, Landing, image1, image2, 
-  image4, image5, image6, image7, image8, image9, image11, image12, image13, image14, image15, image16, image17, image18, image19, image20, image21, image22, image23, MiddleLanding, MiddleHome, MiddleContact, Footer} from './components/views.js'
+/*   image4, image5, image6, image7, image8, image9, image11, image12, image13, image14, image15, image16, image17, image18, image19, image20, image21, image22, image23, */ MiddleLanding, MiddleHome, MiddleContact, Footer} from './components/views.js'
 import { Parallax, ParallaxLayer} from '@react-spring/parallax'
 import { useInView } from 'react-intersection-observer'
 
@@ -23,13 +23,13 @@ return (
         <Landing/>
       </ParallaxLayer>
 
-     <ParallaxLayer offset={0.90} speed={0.8}>
+     {/* <ParallaxLayer offset={0.90} speed={0.8}>
         <img src={image8} alt="" />
       </ParallaxLayer>  
     
       <ParallaxLayer offset={0.85} speed={1.2}>
         <img src={image9} alt="" />
-      </ParallaxLayer>  
+      </ParallaxLayer>   */}
 
       <ParallaxLayer offset={0.95} speed={1.5}>
         <MiddleLanding/>
@@ -49,7 +49,7 @@ return (
         </div>
       </ParallaxLayer>
            
-      <ParallaxLayer  offset={1.95} speed={1} className='pruebita'>
+      {/* <ParallaxLayer  offset={1.95} speed={1} className='pruebita'>
         <img src={image11} alt=""  />
       </ParallaxLayer>
      
@@ -73,7 +73,7 @@ return (
         <figure>
          <img src={image5} alt=""/>
         </figure>
-      </ParallaxLayer>
+      </ParallaxLayer> */}
      
 
         <ParallaxLayer  offset={2} speed={1}>
@@ -83,7 +83,7 @@ return (
         </ParallaxLayer>
         
 
-        <ParallaxLayer  offset={2.95} speed={1}>
+        {/* <ParallaxLayer  offset={2.95} speed={1}>
           <img src={image13} alt="" />
         </ParallaxLayer>
         
@@ -93,14 +93,14 @@ return (
         
         <ParallaxLayer  offset={2.95} speed={2}>
         <img src={image15} alt="" />
-      </ParallaxLayer>
+      </ParallaxLayer> */}
 
         <ParallaxLayer  offset={2.95} speed={1}>
           <MiddleContact/>
         </ParallaxLayer> 
 
 
-        <ParallaxLayer  offset={3.25} speed={1.5}>
+        {/* <ParallaxLayer  offset={3.25} speed={1.5}>
           <img src={image16} alt="" />
         </ParallaxLayer>
 
@@ -122,19 +122,19 @@ return (
 
         <ParallaxLayer  offset={3.25} speed={0.2}>
           <img src={image23} alt="" />
-        </ParallaxLayer>
+        </ParallaxLayer> */}
 
         <ParallaxLayer  offset={3.25} speed={1}>
           <Experience/>
         </ParallaxLayer>
 
-        <ParallaxLayer  offset={4} speed={1.5}>
+        {/* <ParallaxLayer  offset={4} speed={1.5}>
           <img src={image21} alt="" />
         </ParallaxLayer>
 
         <ParallaxLayer  offset={4} speed={2}>
           <img src={image22} alt="" />
-        </ParallaxLayer>
+        </ParallaxLayer> */}
 
         <ParallaxLayer  offset={4} speed={1}>
           <SkillSet/>
